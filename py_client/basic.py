54644a42c25eb8fb1306335f1aea9f8dc9c7af74
prbehere93 <prbehere93@gmail.com>
2022-03-01 00:29:43 +0530

    Update:made my first simple api in Django, essentially just returns a JSON response of some text

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,14 @@
 import requests
 
-endpoint="https://httpbin.org/status/200/"
-endpoint2="https://httpbin.org/anything"
+# endpoint="https://httpbin.org/status/200/"
+# endpoint2="https://httpbin.org/anything"
+
+my_endpoint="http://localhost:8000/api"
 
 #data is sent as form data, the json data is json data
-get_response=requests.get(endpoint2, data={"hey":"naaa"},json={"pratyush":"hello world to me"})
+# get_response=requests.get(endpoint2, data={"hey":"naaa"},json={"pratyush":"hello world to me"})
+# print(get_response.json())
+
+get_response=requests.get(my_endpoint, params={"abc":123}, json={"query":"some wierd text data"})
 print(get_response.json())
+print(get_response.status_code)
