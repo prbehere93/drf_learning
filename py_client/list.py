e23bb4f1c55d45e45694c992939c8a6bc2d1987a
prbehere93 <prbehere93@gmail.com>
2022-03-12 19:02:39 +0530

    added a token based auth

--- py_client/list.py
+++ py_client/list.py
@@ -5,8 +5,8 @@ import requests
 
 endpoint="http://localhost:8000/api/products/" #for fetching the list
 auth_endpoint="http://localhost:8000/api/auth/" #for generating the auth token
-username=input('type your username')
-password=getpass('type your password')
+username=input('type your username\n')
+password=getpass('type your password\n')
 
 auth_response=requests.post(auth_endpoint,json={"username":username,
                                                 "password":password})
@@ -19,3 +19,5 @@ if auth_response.status_code==200: #if the auth is successfull
     }
     get_response=requests.get(endpoint, headers=headers)
     print(get_response.json())
+else:
+    print("Give the correct Credentials")
