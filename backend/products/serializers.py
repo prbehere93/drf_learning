ca9261eae794c31e655cbdc38ffba2e8eb379224
prbehere93 <prbehere93@gmail.com>
2022-03-13 17:05:36 +0530

    modified the ProductSerializer to show Update and the DetailView urls of the object

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,17 +1,30 @@
+from urllib import request
 from rest_framework import serializers
+from rest_framework.reverse import reverse # this is different than the django reverse func
 from .models import Products
 
 class ProductSerializer(serializers.ModelSerializer): #very similar to how you make ModelForms
     my_discount=serializers.SerializerMethodField(read_only=True)
+    edit_url=serializers.SerializerMethodField(read_only=True)
+    url=serializers.HyperlinkedIdentityField(view_name='product-detail', #takes the url name and the lookup field as args
+                                             lookup_field='pk')
     class Meta:
         model=Products
         fields=[
+            'url',
+            'edit_url',
             'title',
             'content',
             'price',
             'sale_price',
             'my_discount'
         ]
+        
+    def get_edit_url(self,obj):
+        request=self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-update", kwargs={'pk':obj.pk}, request=request)
     def get_my_discount(self,obj):
         """Essentially we can do whatever we want with the obj instance here and return what we want to the User"""
         #you have the instance of the model here, i.e. 'obj'
