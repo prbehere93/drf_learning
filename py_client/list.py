0bed535b6d6fd53788f427b356d66d865ec04b07
prbehere93 <prbehere93@gmail.com>
2022-03-16 20:10:35 +0530

    added comments to py_client

--- py_client/list.py
+++ py_client/list.py
@@ -11,7 +11,10 @@ password=getpass('type your password\n')
 auth_response=requests.post(auth_endpoint,json={"username":username,
                                                 "password":password})
 
-
+"""
+You will need to change the client after pagination has been implemented (or create a different endpoint which
+will fetch all results)
+"""
 if auth_response.status_code==200: #if the auth is successfull
     token=auth_response.json()['token']
     headers={
