bf90fdb9e945ef76d0e7167e918c1c4a0e5cf998
prbehere93 <prbehere93@gmail.com>
2022-03-25 16:02:36 +0530

    wrote a product diplay list func which uses the JWT tokens for login and displays the product list of a particular user

--- js_client/client.js
+++ js_client/client.js
@@ -1,3 +1,4 @@
+const contentContainer=document.getElementById("content-container")
 const loginForm= document.getElementById("login-form")
 
 const baseEndPoint= "http://localhost:8000/api"
@@ -21,16 +22,81 @@ function handleLogin(event){
         },
         body:JSON.stringify(loginObjectFormData)
     }
-    fetch(loginEndPoint, options).then(
+    fetch(loginEndPoint, options).then( //promise js thing 
         response=> {
             console.log(response)
             return response.json()
         }
     )
-    .then(x=>{
-        console.log(x)
+    .then(authData=>{
+        handleAuthData(authData,getProductListData) //gets the tokens and calls the getProductlist func
     })
     .catch(err=>{
         console.log('err', err)
     })
 }
+
+
+function handleAuthData(authData, callback){ //function for storing tokens obtained after successful login
+    localStorage.setItem('access',authData.access)
+    localStorage.setItem('refresh', authData.refresh)
+    if (callback){
+        callback()  //essentially calls a func, if there is one
+    }
+}
+
+function isTokenNotValid(jsonData){
+    if (jsonData.code && jsonData.code==="token_not_valid"){
+        //you can run a refresh token here, maybe
+        alert("Please Login again")
+    }
+}
+
+function writeToContainer(data){
+    if (contentContainer){
+        contentContainer.innerHTML+="<pre>"+JSON.stringify(data,null,4)+"</pre>"
+    }
+}
+
+function getFetchOptions(method, body){
+    return{
+        method: method===null ? "GET" : method, //if?else
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization":`Bearer ${localStorage.getItem('access')}`
+        },
+        body:body? body:null //if?else
+    }
+}
+function validateJWTToken() {
+    // fetch
+    const endpoint = `${baseEndpoint}/token/verify/`
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            token: localStorage.getItem('access')
+        })
+    }
+    fetch(endpoint, options)
+    .then(response=>{
+        console.log(response.json())    
+        response.json()
+    })
+    .then(x=> {
+        // refresh token
+    })
+}
+function getProductListData(){ //func to fetch product list from the api and write to 
+    endpoint=`${baseEndPoint}/products/`
+    const options=getFetchOptions() 
+    fetch(endpoint,options)
+    .then(response=>response.json())
+    .then(data=>{
+        writeToContainer(data)
+    })
+}
+
+validateJWTToken()
